e24945b24111d381ec50ba2bf0b50b14e6995488
A. Santos <65362444+lunauii@users.noreply.github.com>
2025-11-13 11:34:38 -0800

    Delete build/Desktop-Debug directory

--- build/Desktop-Debug/.qt/appQSynthesizer_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appQSynthesizer")
-set(working_dir "/home/lunaui/Documents/bcit/comp3931/QSynthesizer")
-set(src_and_dest_list
-    "/home/lunaui/Documents/bcit/comp3931/QSynthesizer/Main.qml"
-    "/home/lunaui/Documents/bcit/comp3931/QSynthesizer/build/Desktop-Debug/QSynthesizer/Main.qml"
-
-)
-set(timestamp_file "/home/lunaui/Documents/bcit/comp3931/QSynthesizer/build/Desktop-Debug/.qt/appQSynthesizer_qml.txt")
